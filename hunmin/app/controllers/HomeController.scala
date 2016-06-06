9c156d60889f76ea94a6c51d281b8eb389af4dc2
hunmin <hunmin@widerplanet.com>
2016-06-06 17:47:10 +0900

    list 가지고 오는 부분 스터디 완료

--- hunmin/app/controllers/HomeController.scala
+++ hunmin/app/controllers/HomeController.scala
@@ -1,24 +1,54 @@
 package controllers
 
 import javax.inject._
-import play.api._
+import dao.UserDAO
+import models.User
 import play.api.mvc._
 
+import scala.concurrent.Future
+
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+
 /**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
+  * This controller creates an `Action` to handle HTTP requests to the
+  * application's home page.
+  */
 @Singleton
-class HomeController @Inject() extends Controller {
+class HomeController @Inject()(userDao : UserDAO) extends Controller {
+
 
   /**
-   * Create an Action to render an HTML page with a welcome message.
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
+    * Create an Action to render an HTML page with a welcome message.
+    * The configuration in the `routes` file means that this method
+    * will be called when the application receives a `GET` request with
+    * a path of `/`.
+    */
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    var outString = "Number is "
+//    val conn = db.getConnection()
+//
+//    try {
+//      val stmt = conn.createStatement()
+//      val rs = stmt.executeQuery("SELECT 9 as testkey")
+//
+//      while (rs.next()) {
+//        outString += rs.getString("testKey")
+//      }
+//    } finally {
+//      conn.close()
+//    }
+
+    Ok(outString)
   }
+
+//  def echo = Action { request =>
+    //    Ok("Got request [" + request + "]")
+//    userDao.all().map( case (users) => Ok(views.html.board.Application.index(users));
+//  }
+
+  def echo = Action.async(
+    userDao.all().map(users => Ok(views.html.board.index("aaaa")(users)))
+  )
+
 
 }
--- hunmin/app/dao/UserDAO.scala
+++ hunmin/app/dao/UserDAO.scala
@@ -1,8 +1,37 @@
 package dao
 
+import javax.inject.Inject
+
+import models.User
+import play.api.db.slick.{HasDatabaseConfigProvider, DatabaseConfigProvider}
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import slick.driver.JdbcProfile
+
+import scala.concurrent.Future
+
 /**
   * Created by kohunmin on 2016. 5. 28..
   */
-class UserDAO {
+class UserDAO @Inject()(protected val dbConfigProvider: DatabaseConfigProvider) extends HasDatabaseConfigProvider[JdbcProfile]{
+  import driver.api._
+
+  private val Users = TableQuery[UserTable]
+
+  def all(): Future[Seq[User]] = db.run(Users.result)
+
+  // _ 는 모두를 의미
+  def insert(user: User): Future[Unit] =
+    db.run(Users += user).map(_ => ())
+
+  private class UserTable(tag: Tag) extends Table[User](tag,"User") {
+
+    def id = column[Int]("ID", O.PrimaryKey)
+    def email = column[String]("EMAIL")
+    def password = column[String]("PASSWORD")
+    def fullname = column[String]("FULLNAME")
+    def isAdmin = column[Boolean]("ISADMIN")
+
+    override def * = (id,email,password,fullname,isAdmin) <> (User.tupled , User.unapply _)
+  }
 
 }
